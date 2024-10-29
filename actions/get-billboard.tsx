80cad1782b9f2ae446059563dba93a7bc9d7eef7
denisdiv <djw19910815@gmail.com>
2024-10-29 20:05:56 +0800

    change4

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -5,7 +5,7 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 const getBillboard = async (id: string): Promise<Billboard> => {
     // const res = await fetch(URL);
     // const res = await fetch(`${URL}?t=${Date.now()}`);
-    const res = await fetch(`${URL}/${id}`);
+    const res = await fetch(`${URL}/${id}?t=${Date.now()}`);
 
     return res.json();
 }
