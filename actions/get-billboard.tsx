e2eec2ba39fdb42f727ddaad7bd780c7f2769933
Div-Denis <82740084+Div-Denis@users.noreply.github.com>
2025-06-19 22:53:21 +0800

    Update get-billboard.tsx

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -2,12 +2,20 @@ import { Billboard } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
-const getBillboard = async (id: string): Promise<Billboard> => {
+const getBillboard = async (id: string): Promise<Billboard | null> => {
     // const res = await fetch(URL);
     // const res = await fetch(`${URL}?t=${Date.now()}`);
-    const res = await fetch(`${URL}/${id}?t=${Date.now()}`);
-
-    return res.json();
+    try {
+        const res = await fetch(`${URL}/${id}?t=${Date.now()}`);
+        if(!res.ok) {
+            throw new Error(`Failed to fetch billboard: ${res.status}`);
+            return null
+        }
+        return await res.json()
+    } catch (err) {
+        console.error(err);
+        return null
+    }
 }
 
 export default getBillboard
