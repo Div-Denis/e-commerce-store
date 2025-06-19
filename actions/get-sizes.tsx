2bdfc96b95aadf6dc0ac66071285b64242717be6
Div-Denis <82740084+Div-Denis@users.noreply.github.com>
2025-06-19 22:55:40 +0800

    Update get-sizes.tsx

--- actions/get-sizes.tsx
+++ actions/get-sizes.tsx
@@ -3,10 +3,19 @@ import { Size } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`;
 
 const getSizes = async (): Promise<Size[]> => {
-    // const res = await fetch(URL);
-    const res = await fetch(`${URL}?t=${Date.now()}`);
-
-    return res.json();
+    try {
+        const res = await fetch(`${URL}?t=${Date.now()}`);
+        if (!res.ok) {
+            // 可以在这里记录错误日志
+            return [];
+        }
+        return await res.json();
+    } catch (err) {
+        // 可以在这里记录异常
+        console.error(err);
+        
+        return [];
+    }
 }
 
 export default getSizes
