0578e5dfbf9043aa59280666dea5a4c46bf1eae2
Div-Denis <82740084+Div-Denis@users.noreply.github.com>
2025-06-19 22:54:42 +0800

    Update get-colors.tsx

--- actions/get-colors.tsx
+++ actions/get-colors.tsx
@@ -3,10 +3,19 @@ import { Color } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`;
 
 const getColors = async (): Promise<Color[]> => {
-    // const res = await fetch(URL);
-    const res = await fetch(`${URL}?t=${Date.now()}`);
-
-    return res.json();
-}
+    try {
+        const res = await fetch(`${URL}?t=${Date.now()}`);
+        if (!res.ok) {
+            // 这里可以上报错误日志
+            return [];
+        }
+        return await res.json();
+    } catch (err) {
+        // 这里可以上报异常
+        console.error(err);
+        
+        return [];
+    }
+};
 
 export default getColors
