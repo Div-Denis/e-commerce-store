ea3607f70d7e829f7c901e6993a451d49e20d4d6
Div-Denis <82740084+Div-Denis@users.noreply.github.com>
2025-06-19 22:54:58 +0800

    Update get-product.tsx

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -2,12 +2,20 @@ import { Product } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-const getProduct = async (id: string): Promise<Product> => {
-    // const res = await fetch(URL);
-    // const res = await fetch(`${URL}?t=${Date.now()}`);
-    const res = await fetch(`${URL}/${id}?t=${Date.now()}`);
+const getProduct = async (id: string): Promise<Product | null> => {
+    try {
+        const res = await fetch(`${URL}/${id}?t=${Date.now()}`);
+        if (!res.ok) {
+            // 可以在这里 log 或打点
+            return null;
+        }
+        return await res.json();
+    } catch (err) {
+        // log error if needed
+        console.error(err);
+        
+        return null;
+    }
+};
 
-    return res.json();
-}
-
-export default getProduct
+export default getProduct;
